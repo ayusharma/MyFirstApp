d914c84dac266861f8470046ef68d6323b25e5b8
ayush sharma <ayush.aceit@gmail.com>
2015-10-06 19:02:13 +0530

    min css

--- compressor.sh
+++ compressor.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 java -jar yuicompressor-2.4.7.jar  "scripts/effects.js" -o "scripts/effects.min.js"
+java -jar yuicompressor-2.4.7.jar  "css/style.css" -o "css/style.min.css"
 exit 0;
